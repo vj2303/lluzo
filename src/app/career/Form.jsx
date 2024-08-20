0a13af1dbcd75d636c5d5bc9cf14da98f6a6cf0a
Vishnu Jangid <103036831+vj2303@users.noreply.github.com>
2024-08-20 18:09:52 +0530

    Update Form.jsx

--- src/app/career/Form.jsx
+++ src/app/career/Form.jsx
@@ -41,9 +41,9 @@ const Form = () => {
       })
       reset()
       Swal.fire({
-        title: 'Thank you for submitting. Our team will get in touch with you shortly. In the meantime, you can call or WhatsApp 8879949404',
+       title: 'Thank you for submitting. Our team will get in touch with you shortly. In the meantime, you can call or WhatsApp us at:',
         icon: 'success',
-        html: '',
+        html: '<a href="https://api.whatsapp.com/send?phone=918879949404" target="_blank" style="color: #007bff; text-decoration: none;">+91-8879949404</a>',
         showConfirmButton: true,
         allowOutsideClick: true,
         confirmButtonText: "Ok"
